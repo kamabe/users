60bf155c1508f0814b66b16bc0f7b20d09b2fb0f
Kayla Townsend <kayla.townsend@mosaik.com>
2012-11-29 15:35:28 -0600

    Removed to_sym from group name

--- recipes/sysadmins.rb
+++ recipes/sysadmins.rb
@@ -21,12 +21,12 @@
 # Searches data bag "users" for groups attribute "sysadmin".
 # Places returned users in Unix group "sysadmin" with GID 2300.
 node[:users][:groups].each do |group|
-  users_manage node[:users][group.to_sym][:name] do
-    data_bag     node[:users][group.to_sym][:data_bag]     if node[:users][group.to_sym].key?(:data_bag)
-    search_group node[:users][group.to_sym][:search_group] if node[:users][group.to_sym].key?(:search_group)
-    group_name   node[:users][group.to_sym][:group_name]   if node[:users][group.to_sym].key?(:group_name)
-    group_id     node[:users][group.to_sym][:group_id]
-    cookbook     node[:users][group.to_sym][:cookbook]     if node[:users][group.to_sym].key?(:cookbook)
+  users_manage node[:users][group][:name] do
+    data_bag     node[:users][group][:data_bag]     if node[:users][group].key?(:data_bag)
+    search_group node[:users][group][:search_group] if node[:users][group].key?(:search_group)
+    group_name   node[:users][group][:group_name]   if node[:users][group].key?(:group_name)
+    group_id     node[:users][group][:group_id]
+    cookbook     node[:users][group][:cookbook]     if node[:users][group].key?(:cookbook)
     action       [ :remove, :create ]
   end
 end
